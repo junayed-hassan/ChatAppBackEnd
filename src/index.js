9cac015f3d94203e86a3297b331b6fc6cfaeb443
junayed-hassan <junayedhassan102045@gmail.com>
2025-01-25 15:57:14 +0600

    up

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 // Import routes
 import { connectDB } from './lib/db.js';
@@ -19,6 +20,12 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 // Routes
 app.use("/api/auth", authRoutes);
